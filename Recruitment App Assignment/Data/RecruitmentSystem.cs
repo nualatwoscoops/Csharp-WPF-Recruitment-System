bb18507e44c6f12d520ffdcd8d23098b653eb0a6
Nuala <malonenuala@gmail.com>
2025-10-30 17:50:24 +0800

    Initial commit of Recruitment System

---
+++ Recruitment App Assignment/Data/RecruitmentSystem.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Navigation;
+
+namespace Recruitment_App_Assignment.Data
+{
+    public class RecruitmentSystem
+    {
+        //holds all contractor objects 
+        public List<Contractor> AllContractors { get; set; } = new List<Contractor>();
+
+        //holds all jobs which the agency manages
+        public List<Job> AllJobs { get; set; } = new List<Job>();
+
+        public List<Contractor> GetAllContractors()
+            { return AllContractors; }
+
+        //add and remove contractors here
+        public void RemoveContractor(Contractor oldContractor)
+        {
+            AllContractors.Remove(oldContractor);
+        }
+
+        public void AddContractor(Contractor newContractor)
+        {
+            AllContractors.Add(newContractor);   
+        }
+        public void AddJob(Job newJob)
+        {
+            AllJobs.Add(newJob);
+        }
+
+        public Contractor Search (int id) 
+        { 
+            for (int i=0;i<AllContractors.Count;++i) 
+            {
+                if (AllContractors[i].ID == id) 
+                { 
+                    return AllContractors[i];
+                }
+            }
+            return null;    
+        }
+    }
+}
